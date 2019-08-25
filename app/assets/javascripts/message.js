8452ab32d361b24ca6cf31625b8219183e725c49
homarinn <homarinn1230@gmail.com>
2019-08-25 14:11:44 +0900

    format js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,48 +1,60 @@
-$(function(){
-  let buildHTML = (message) => {
+$(function() {
+  let buildHTML = message => {
     let messageInfo = `<div class="message__info">
-                         <p class="message__info__user-name">${message.user_name}<p>
-                         <p class="message__info__daytime">${message.created_at}<p>
-                       </div>`
-    let messageText = message.content ? `<p class="message__text">${message.content}</p>` : ""
-    let messageImage = message.image.url ? `<img src="${message.image.url}" width="200" height="200">` : ""
-    let html = `<div class="message" data-group-id="${message.group_id}" data-message-id="${message.id}">
+                         <p class="message__info__user-name">${
+                           message.user_name
+                         }<p>
+                         <p class="message__info__daytime">${
+                           message.created_at
+                         }<p>
+                       </div>`;
+    let messageText = message.content
+      ? `<p class="message__text">${message.content}</p>`
+      : "";
+    let messageImage = message.image.url
+      ? `<img src="${message.image.url}" width="200" height="200">`
+      : "";
+    let html = `<div class="message" data-group-id="${
+      message.group_id
+    }" data-message-id="${message.id}">
                   ${messageInfo}
                   ${messageText}
                   ${messageImage}
-                </div>`
+                </div>`;
     return html;
-    };
+  };
 
-  $("#new_message").on("submit", function(e){
+  $("#new_message").on("submit", function(e) {
     e.preventDefault();
     let formData = new FormData(this);
-    let url = $(this).attr('action');
+    let url = $(this).attr("action");
 
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
       contentType: false
     })
-    .done(function(message){
-      let html = buildHTML(message);
-      $(".messages").append(html).animate({scrollTop: $(".messages")[0].scrollHeight}, 'fast');
-      let last_message = $("#lastmessage_" + message.group_id);
-      if (message.content) {
-        last_message.text(message.content);
-      } else {
-        last_message.text("画像が投稿されています。");
-      }
-      $(".form__input-box__input-text").val("");
-      $("#message_image").val("");
-      $(".form__submit").prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    });
+      .done(function(message) {
+        let html = buildHTML(message);
+        $(".messages")
+          .append(html)
+          .animate({ scrollTop: $(".messages")[0].scrollHeight }, "fast");
+        let last_message = $("#lastmessage_" + message.group_id);
+        if (message.content) {
+          last_message.text(message.content);
+        } else {
+          last_message.text("画像が投稿されています。");
+        }
+        $(".form__input-box__input-text").val("");
+        $("#message_image").val("");
+        $(".form__submit").prop("disabled", false);
+      })
+      .fail(function() {
+        alert("error");
+      });
   });
 
   let reloadMessages = () => {
@@ -52,30 +64,32 @@ $(function(){
 
     $.ajax({
       url: url,
-      type: 'GET',
-      dataType: 'json',
-      data: {message_id: last_message_id}
+      type: "GET",
+      dataType: "json",
+      data: { message_id: last_message_id }
     })
-    .done(function(messages) {
-      if (messages.length !== 0){
-        let insertHTML = '';
-        messages.forEach(function(message){
-          insertHTML += buildHTML(message);
-          let last_message = $("#lastmessage_" + message.group_id);
-          if (message.content) {
-            last_message.text(message.content);
-          } else {
-            last_message.text("画像が投稿されています。");
-          } 
-        });
-        $(".messages").append(insertHTML).animate({scrollTop: $(".messages")[0].scrollHeight}, 'fast');
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          let insertHTML = "";
+          messages.forEach(function(message) {
+            insertHTML += buildHTML(message);
+            let last_message = $("#lastmessage_" + message.group_id);
+            if (message.content) {
+              last_message.text(message.content);
+            } else {
+              last_message.text("画像が投稿されています。");
+            }
+          });
+          $(".messages")
+            .append(insertHTML)
+            .animate({ scrollTop: $(".messages")[0].scrollHeight }, "fast");
+        }
+      })
+      .fail(function() {
+        alert("error");
+      });
   };
-  if(document.URL.match(/messages/)){
+  if (document.URL.match(/messages/)) {
     setInterval(reloadMessages, 3000);
   }
 });
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,59 +1,70 @@
-$(function(){
-  let search_result = $("#user-search-result")
-  let group_users = $("#chat-group-users")
-  let group_user_ids = $.makeArray(group_users.children().map(function(i, value){
-    return $(value).data("user-id");
-  }));
+$(function() {
+  let search_result = $("#user-search-result");
+  let group_users = $("#chat-group-users");
+  let group_user_ids = $.makeArray(
+    group_users.children().map(function(i, value) {
+      return $(value).data("user-id");
+    })
+  );
 
-  let appendSearchUser = (user) => {
+  let appendSearchUser = user => {
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                </div>`
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${
+                    user.id
+                  }" data-user-name="${user.name}">追加</div>
+                </div>`;
     search_result.append(html);
   };
 
-  let appendNoUser = (message) => {
+  let appendNoUser = message => {
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${message}</p>
-                </div>`
+                </div>`;
     search_result.append(html);
   };
 
-  let appendGroupMember = (member) => {
-    let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${member.userId}' data-user-id="${member.userId}">
-                  <input name='group[user_ids][]' type='hidden' value='${member.userId}'>
+  let appendGroupMember = member => {
+    let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${
+      member.userId
+    }' data-user-id="${member.userId}">
+                  <input name='group[user_ids][]' type='hidden' value='${
+                    member.userId
+                  }'>
                   <p class='chat-group-user__name'>${member.userName}</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-                </div>`
+                </div>`;
     group_users.append(html);
   };
 
-  $("#user-search-field").on("keyup", function(){
-    let input = $(this).val()
-    
+  $("#user-search-field").on("keyup", function() {
+    let input = $(this).val();
+
     $.ajax({
-      url: '/users',
+      url: "/users",
       type: "GET",
-      dataType: 'json',
-      data: { keyword: input, ids: group_user_ids }
-    })
-    .done(function(users){
-      search_result.empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendSearchUser(user);
-        });
-      } else {
-        appendNoUser('一致するユーザーが見つかりません');
+      dataType: "json",
+      data: {
+        keyword: input,
+        ids: group_user_ids
       }
     })
-    .fail(function(){
-      alert('ユーザー検索に失敗しました');
-    });
+      .done(function(users) {
+        search_result.empty();
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            appendSearchUser(user);
+          });
+        } else {
+          appendNoUser("一致するユーザーが見つかりません");
+        }
+      })
+      .fail(function() {
+        alert("ユーザー検索に失敗しました");
+      });
   });
 
-  search_result.on("click", ".user-search-add", function(e){
+  search_result.on("click", ".user-search-add", function(e) {
     let target = $(e.target);
     let member = target.data();
     target.parent().remove();
@@ -61,10 +72,10 @@ $(function(){
     appendGroupMember(member);
   });
 
-  group_users.on("click", ".user-search-remove", function(e){
+  group_users.on("click", ".user-search-remove", function(e) {
     let target = $(e.target).parent();
-    group_user_ids = group_user_ids.filter(function(user_id){
-      return user_id !== target.data("user-id")
+    group_user_ids = group_user_ids.filter(function(user_id) {
+      return user_id !== target.data("user-id");
     });
     target.remove();
   });
